98a2e4a2d3c64d491900edc3ec762cf0bd8cdc5c
Lucas Sowul <sowul.lukas@gmail.com>
2023-08-19 15:14:55 +0200

    xyz
    xyxzz

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Register from './Register';
-import Login from './Login';
+/*import Login from './Login';*/
 class App extends Component {
   render () {
     return <Register/>
