ea16ad1388197ac814f7469d944592847264db03
Lucas Sowul <sowul.lukas@gmail.com>
2024-01-11 18:27:48 +0100

    Repaired
    Working once again !!!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-/*import Register from './Register';*/
-import Login from './Login';
+import Register from './Register';
 
 class App extends Component {
   render () {
-    return <Login/>
+    return <Register/>
   }
 }
 export default App;
