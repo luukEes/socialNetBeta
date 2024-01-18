bdb5ae36f69d0940de9ea6c1063eaae3e66b2040
Lucas Sowul <sowul.lukas@gmail.com>
2024-01-18 19:56:32 +0100

    DashBoard page
    Added return page after login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from './Login';
-import HomePage from './HomePage'; // Assume you have a component for the home page
 
 class App extends Component {
   constructor() {
@@ -15,20 +14,13 @@ class App extends Component {
     // Assuming you have some logic to determine successful login
     // For demonstration purposes, I'm just setting the state to true
     this.setState({ isLoggedIn: true });
-    window.location.href = 'http://192.168.1.23:8081/dashboard';
+    window.location.href = './dashboard.html';
   };
 
   render() {
     return (
       <div>
-        {this.state.isLoggedIn ? (
-          // If logged in, open a new window or navigate to another page
-          // You should replace this with the actual logic to open a new window or navigate
-          <HomePage />
-        ) : (
-          // If not logged in, render the Login component
-          <Login onLoginSuccess={this.handleLoginSuccess} />
-        )}
+        <Login onLoginSuccess={this.handleLoginSuccess} />
       </div>
     );
   }
